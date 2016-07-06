66532393c7229f526cbfde55504becc7914ea91c
slrc1 <info@slrc1.comli.com>
2016-07-06 09:30:27 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-os.system("wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2")
-os.system("tar -vxjf phantomjs-2.1.1-linux-i686.tar.bz2")
-os.system("mv phantomjs-2.1.1-linux-i686/bin/phantomjs /")
+#os.system("wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2")
+#os.system("tar -vxjf phantomjs-2.1.1-linux-i686.tar.bz2")
+#os.system("mv phantomjs-2.1.1-linux-i686/bin/phantomjs /")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
