3606e4abbcce3aa323ed7ca8ccdcac1111d8e768
slrc1 <info@slrc1.comli.com>
2016-07-06 09:32:55 +0530

    Add files via upload

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,10 +11,4 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^compileasm', hello.views.compileasm, name='compileasm'),
-    url(r'^runasm', hello.views.runasm, name='runasm'),
-    url(r'^ts2wmv', hello.views.ts2wmv, name='ts2wmv'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^save', hello.views.save, name='save'),
 ]
